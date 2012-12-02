6dccf2b109ae61d28486739e647504dcd77ba787
Konstantin Stepanov <me@kstep.me>
2012-12-02 20:39:15 +0300

    Remove underscorejs from tests dependencies

--- tests/_testacular.conf.js
+++ tests/_testacular.conf.js
@@ -1,4 +1,4 @@
-files = [JASMINE, JASMINE_ADAPTER, '../js/_libs/jquery.min.js', '../js/_libs/angular.js', '../js/_libs/angular-*.js', 'libs/angular-mocks.js', '../js/underscore-min.js', '../js/application-min.js', '**/*Spec.js', '**/*Spec.coffee'];
+files = [JASMINE, JASMINE_ADAPTER, '../js/_libs/jquery.min.js', '../js/_libs/angular.js', '../js/_libs/angular-*.js', 'libs/angular-mocks.js', '../js/application-min.js', '**/*Spec.js', '**/*Spec.coffee'];
 browsers = ['Firefox', 'Chrome', 'Opera'];
 autoWatch = true;
 preprocessors = {
